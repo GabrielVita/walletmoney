81d4f7b1aa2850134294af6ab02d14c3eba0704a
Gabriel Vita <gabriel.vita83@gmail.com>
2023-07-25 18:33:38 -0300

    header, table, summary

---
+++ wallet/src/components/Historic/index.tsx
@@ -0,0 +1,32 @@
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
+
+export function Historic(){
+    return(
+        <TransactionsContainer>
+            <TransactionsTable>
+                <tbody>
+                    <tr>
+                        <td width="50%">Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighlight variant="income">
+                                R$ 10.000,00
+                            </PriceHighlight>
+                        </td>
+                        <td>Venda</td>
+                        <td>25/07/2023</td>
+                    </tr>
+                    <tr>
+                        <td>Carro</td>
+                        <td>
+                            <PriceHighlight variant="outcome">
+                                - R$ 10.000,00
+                            </PriceHighlight>
+                        </td>
+                        <td>Manutenção</td>
+                        <td>25/07/2023</td>
+                    </tr>
+                </tbody>
+            </TransactionsTable>
+        </TransactionsContainer>
+    );
+}
---
+++ wallet/src/pages/Transactions/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header";
+import { Historic } from "../../components/Historic";
+import { Summary } from "../../components/Summary";
+
+export function Transaction(){
+    return(
+        <div>
+            <Header/>
+            <Summary/>
+
+            <Historic/>
+
+        </div>
+    )
+}
