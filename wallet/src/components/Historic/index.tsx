38c741fea0457c7f094c0b85fa77f169a61c5524
Gabriel Vita <gabriel.vita83@gmail.com>
2023-08-07 18:16:28 -0300

    format data and hooks calculate

--- wallet/src/components/Historic/index.tsx
+++ wallet/src/components/Historic/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { SearchForm } from "../SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Historic(){
     const {transactions} = useContext(TransactionsContext);
@@ -17,11 +18,12 @@ export function Historic(){
                                 <td width="50%">{transaction.description}</td>
                                 <td>
                                     <PriceHighlight variant={transaction.type}>
-                                        {transaction.price}
+                                        {transaction.type === 'outcome' && '- '}
+                                        {priceFormatter.format(transaction.price)}
                                     </PriceHighlight>
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                             </tr>
                         )
                     })}
